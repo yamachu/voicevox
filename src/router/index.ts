aea65ca4fe6209f047d82c2845f381846ca144a6
yamachu <yamachu.dev@gmail.com>
2023-12-20 22:14:04 +0900

    ログインのページ忘れがち

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import EditorHome from "../views/EditorHome.vue";
+import LoginRoot from "../views/LoginRoot.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/home",
     component: EditorHome,
     props: (route) => ({ projectFilePath: route.query["projectFilePath"] }),
+  },
+  {
+    path: "/",
+    component: LoginRoot,
   },
 ];
 
---
+++ src/views/LoginRoot.vue
@@ -0,0 +1,11 @@
+<template>
+  <a :href="`${host}/.auth/login/aad`">Login With AAD</a>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const host = computed(
+  () => JSON.parse(import.meta.env.VITE_DEFAULT_ENGINE_INFOS)[0].host
+);
+</script>
